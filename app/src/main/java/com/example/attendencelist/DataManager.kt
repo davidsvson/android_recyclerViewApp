96a6dcaa771609c396527de4883405835ab27176
David Svensson <davidsvson@gmail.com>
2022-11-21 13:20:03 +0100

    first commit

---
+++ app/src/main/java/com/example/attendencelist/DataManager.kt
@@ -0,0 +1,18 @@
+package com.example.attendencelist
+
+object DataManager {
+    val students = mutableListOf<Student>()
+
+    init {
+        createMockData()
+    }
+
+    fun createMockData() {
+        students.add(Student("David", "APP22" , true))
+        students.add(Student("Lois", "APP22"))
+        students.add(Student("Laura", "APP22"))
+        students.add(Student("Susan", "APP22"))
+        students.add(Student("Jacob", "APP22", true))
+        students.add(Student("Per", "APP22"))
+    }
+}
